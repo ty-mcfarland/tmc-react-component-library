986032fb53d1974bf5bd9124078a6e103c12a6ff
bigweezyproject <bigweezyproject@gmail.com>
2020-04-23 16:35:53 -0400

    init
    - initialization with typescript and webpack
    - updated build

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+
+module.exports = {
+    mode: "production",
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".js", ".ts", ".tsx", 'scss']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.scss$/,
+                include: /src/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.template.html')
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+    }
+
+    // // When importing a module whose path matches one of the following, just
+    // // assume a corresponding global variable exists and use that instead.
+    // // This is important because it allows us to avoid bundling all of our
+    // // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
+};
